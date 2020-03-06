a6f0f8f06c4b401987fc02d58d45382f947d8731
s15801 <s15801@pjwstk.edu.pl>
2020-03-06 11:51:16 +0100

    first commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Tutorial11
+    {
+
+         public  static async Task Main(string[] args)
+        {
+          
+            //var websiteUrl = args.Length > 0 ? args[0] : throw new ArgumentNullExcept
+            string websiteUrl = "http://pja.edu.pl";
+
+            var x = websiteUrl;
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync(websiteUrl);
+
+
+
+            if (response.IsSuccessStatusCode)
+            {
+                var htmlContent = await response.Content.ReadAsStringAsync();
+                var regex = new Regex(" [a-z]+[a-z0-9-]*@[a-z]+\\.[a-z]+", RegexOptions.IgnoreCase);
+                var emailAddresses = regex.Matches(htmlContent);
+                foreach (var match in emailAddresses)
+                {
+
+                    Console.WriteLine(match.ToString());
+                }
+            }
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
